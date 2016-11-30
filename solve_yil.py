9dac55b90e6e5630319dd3ce890ea804e282f5f5
Yilun Chen <yilunc@berkeley.edu>
2016-11-29 17:49:48 -0800

    adding code

--- solve_yil.py
+++ solve_yil.py
@@ -1,7 +1,7 @@
-import os
+import os, random
 
-def parse(file_path):
-  scores = []
+def parse_instance(file_path):
+  horses = []
   adj = {}
   with open(file_path, 'rb') as f:
     lines = f.readlines()
@@ -9,13 +9,13 @@ def parse(file_path):
       line = lines[i+1].split()
       if len(line):
         adj[i] = []
-        scores.append(line[i])
+        horses.append(line[i])
         for j in range(len(line)):
             if j != i:
               adj[i].append(line[j])
             else:
               adj[i].append(-1)
-  return adj, scores
+  return adj, horses
 
 def write_solution(solution):
   with open('output', 'wb') as f:
@@ -25,27 +25,41 @@ def write_solution(solution):
       f.write("{0}; ".format(path[-1]))
     f.write("\n")
 
-
-
 #TODO
-def solve_instance(instance):
+def solve_instance_compression(instance):
   adj = instance[0]
-  scores = instance[1]
+  horses = instance[1]
   best_solution = []
 
   edges = []
   for h in adj:
     for i in range(len(adj[horse])):
       if adj[horse][i]:
-        adges.append((horse,i))
+        edges.append((horse,i))
 
+  edge_to_compress = edges[int(random.randint()*len(edges))]
 
+  return best_solution
+
+def solve_instance_greedy(instance):
+  adj = instance[0]
+  horses = instance[1]
+  solutions = []
+  best_solution = []
+
+  edges = []
+  for h in adj:
+    for i in range(len(adj[horse])):
+      if adj[horse][i]:
+        edges.append((horse,i))
+
+  horses[int(random.randint()*len(edges))]
 
   return best_solution
 
 def solve():
   for file in os.listdir("cs170_final_inputs"):
     if file.endswith(".in"):
-        instance = parse("cs170_final_inputs/{0}".format(str(file)))
+        instance = parse_instance("cs170_final_inputs/{0}".format(str(file)))
         solution = solve_instance(instance)
         write_solution(solution)
